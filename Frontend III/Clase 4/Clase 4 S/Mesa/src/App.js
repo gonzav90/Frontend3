58b37461293da6e1f28a30f9016e370b3346bd6e
gonzav90 <gonzalovolpe@hotmail.com>
2022-08-23 16:16:03 -0300

    primera semana front3

---
+++ Frontend III/Clase 4/Clase 4 S/Mesa/src/App.js
@@ -0,0 +1,12 @@
+import imagen from './img/satisfied.png';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <img src={imagen} alt="imagen" />
+    </div>
+  );
+}
+
+export default App;
